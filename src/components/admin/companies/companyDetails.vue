55ab5171e6d4d69c87ce97e17055612af7816ab1
michel06485 <michel061004@gmail.com>
2025-04-08 17:27:33 -0500

    diseño

--- src/components/admin/companies/companyDetails.vue
+++ src/components/admin/companies/companyDetails.vue
@@ -1,9 +1,8 @@
 <template>
+
   <section class="max-w-4xl mx-auto mt-4 p-6 bg-white rounded-lg shadow-md">
-    
     <!-- Contenedor principal con información y botones al lado -->
     <div class="w-full flex flex-col md:flex-row justify-between p-8 gap-6" style="border-radius: 0 0 8px 8px;">
-      
       <!-- Información de la empresa -->
       <div class="flex-1">
         <h2 class="text-2xl font-bold text-[#2A5CAA] mb-6 text-center">Información de la Empresa</h2>
@@ -30,16 +29,15 @@
           </div>
         </div>
       </div>
-
+      
       <!-- Botones al lado derecho -->
       <div class="flex flex-col gap-4 md:w-1/5 w-full md:items-end mt-10">
         <button class="w-full transition duration-300 ease-in-out hover:drop-shadow-lg hover:scale-105 bg-[#2A5CAA] text-white px-4 py-2 rounded-md hover:bg-blue-700">Editar</button>
         <button class="w-full transition duration-300 ease-in-out hover:drop-shadow-lg hover:scale-105 bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"> Eliminar </button>
-
       </div>
-
     </div>
   </section>
+    
 </template>
 
 
